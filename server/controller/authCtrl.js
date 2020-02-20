0896d9521f405895d3b77d198e8028186ee2cb72
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-02-20 11:51:24 -0700

    finishes the auth controller file with handlers

--- server/controller/authCtrl.js
+++ server/controller/authCtrl.js
@@ -19,5 +19,31 @@ module.exports = {
                 res.status(401).send('Incorrect email and/or password')
             }
         }
+    },
+    register: async (req, res, next) => {
+        const db = req.app.get('db')
+        const {username, password, email } = req.body
+        const foundUser = await db.select_user(email);
+        if(foundUser.length){
+            res.status(409).send('That user already exists! Please use an alternate email.')
+        } else {
+            const saltRounds = 12;
+            bcrypt.genSalt(saltRounds).then(salt => {
+                bcrypt.hash(password, salt).then( hashedPassword => {
+                    db.create_user([username, email, hashedPassword])
+                    .then(([user]) => {
+                        req.session.user = user;
+                        res.status(200).send(req.session.user)
+                    })
+                })
+            })
+        }
+    },
+    logout: (req, res, next) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    },
+    userSession: (req, res, next) => {
+        res.status(200).send(req.session.user)
     }
 }
